c1084fe8f985502732132e0db8b9ddf71fda3428
Tasha852209 <112940506+Tasha852209@users.noreply.github.com>
2024-03-06 11:10:34 +0000

    Deploying to gh-pages from @ Tasha852209/Teachers-App@15f3bdbfeb8da4f4859565b263eabbad9f43d811 🚀

---
+++ static/js/532.4364111f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[532],{532:function(e,t,r){r.r(t);r(791);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"TeachersPage"})}}}]);
+//# sourceMappingURL=532.4364111f.chunk.js.map
